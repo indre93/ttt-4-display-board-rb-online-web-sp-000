1ff2ad078eae12cf0ebdbcf460dceb2d5a061d61
Indre Bakeer <indre.bakeer@gmail.com>
2018-12-07 22:48:02 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,14 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = ["X", "O", "X", "O", "X", "X", "O", "O", "X"]
+
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
